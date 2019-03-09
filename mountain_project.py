8b163e8b53a740f0ecb6ea25034403d744f85c08
bkmd11 <bkendall11@gmail.com>
2019-03-09 16:18:30 -0500

    I got my loop to work

--- mountain_project.py
+++ mountain_project.py
@@ -51,16 +51,7 @@ def link_finder(web_address):
         climbing_links = climb_finder(page_links)
         return 'climb', climbing_links
 
-
-# Checks for error in link placemant and corrects it
-def list_swap(climbing_list, area_list):
-    for link in climbing_list:
-        if '/area/' in link:
-            area_list.append(link)   
-            climbing_list.remove(link)
-    return climbing_list, area_list
-
-            
+           
 # Setting global variables
 sub_area_links = []
 sub_sub_area_links = []
@@ -69,68 +60,47 @@ climb_links = []
 # Makes a requests obj and parses to BS
 string, areas = link_finder('https://www.mountainproject.com/area/105929413/pawtuckaway')
 
-### I need to make a while loop to go through a whole state
-
-# Gets me a list of sub areas
-for link in areas:
-    x,y = link_finder(link)
-    if x == 'area':
-        sub_area_links += y
-    else:
-        climb_links += y
-
-### I need to actually analyze what this does
-climb_links, sub_area_links = list_swap(climb_links, sub_area_links)
-
-for link in sub_area_links: 
-    x,y = link_finder(link)
-    if x == 'area':
-        sub_sub_area_links += y
-    elif x == 'climb':
-        climb_links += y
-
-climb_links, sub_sub_area_links = list_swap(climb_links, sub_sub_area_links)
-
-sys.exit()  # I have this here because idk how to debug this correctly
+# Loops through every area and sub area
+while True:
+    for link in areas:
+        x, y = link_finder(link)
+        if x == 'area':
+            sub_area_links += y
+        else:
+            climb_links += y
+    areas = sub_area_links
+    if len(areas) == 0:
+        break
+    sub_area_links = []
         
 for climb in climb_links:
-    try:
-        url = climb
-        res = requests.get(url)
-        res.raise_for_status()
+    url = climb
+    res = requests.get(url)
+    res.raise_for_status()
 
-        climb_text = BeautifulSoup(res.text, 'html.parser')
+    climb_text = BeautifulSoup(res.text, 'html.parser')
 
-        # This searches for climbs in each area
-        text_to_search = climb_text.select('.fr-view')    # Description
+    # This searches for climbs in each area
+    text_to_search = climb_text.select('.fr-view')    # Description
 
-        # Makes a regex to find
-        # Needs to search for more variations
-        climbing_term = re.compile(r'off width|off-width|chimney', re.I) 
-        awesome_climb = climbing_term.search(str(text_to_search))
+    # Makes a regex to find
+    # Needs to search for more variations
+    climbing_term = re.compile(r'off width|off-width|chimney', re.I) 
+    awesome_climb = climbing_term.search(str(text_to_search))
 
-        # If regex is found on page do the thing.
-        # Will be changed to make a link in a file
-        if awesome_climb is not None:
-            print(climb)
-        else:
-            continue
-    except:
+    # If regex is found on page do the thing.
+    # Will be changed to make a link in a file
+    if awesome_climb is not None:
+        print(climb)
+    else:
         continue
+
 
 
 """ 
     Current short comings, it searches the entire page for my regex, resulting
-    in climbs that are located by a chminey to show up. Also climbing areas
-    show up in my list if they are described with one of my regex. I only want
-    routes to show up.
-
-    Other than that it looks good. Moving forward I want to try expanding to
-    go through the larger areas or the entire state.
+    in climbs that are located by a chminey to show up.
 
     and again, organize by location or grade.
     And market it to make millions
-
-    It doesnt go all the way to the end of the areas to get to climbs if there
-    are more than two steps.
 """
