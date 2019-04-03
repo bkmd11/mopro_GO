3f5e98a6c87bf3132f502cfc9882da3efc445167
bkmd11 <bkendall11@gmail.com>
2019-04-03 11:06:38 -0400

    added to my route_finder tests

--- scraper_tests/test_route_finder.py
+++ scraper_tests/test_route_finder.py
@@ -5,20 +5,25 @@ import route_finder
 
 
 class TestRouteFinder(unittest.TestCase):
-    def test_grade_regex(self):
+    def test_grade_regex_true(self):
         result = route_finder.regex('5.11')
-
         self.assertIsNotNone(result.search('5.11A '))
+
+    def test_grade_regex_false(self):
+        result = route_finder.regex('5.5')
+        self.assertIsNone(result.search('5.12 '))
 
     def test_grade_regex_escape_character(self):
         result = route_finder.regex('5\.11')
         self.assertIsNone(result.search('105911730'))
 
-    def TestFindByCriteria(self):
-        list_of_climbs = ['pawtuckaway', 'rumney']
-        result = route_finder.find_by_criteria(list_of_climbs)
+    def test_area_regex_true(self):
+        result = route_finder.regex('pawtuckaway')
+        self.assertIsNotNone(result.search('pawtuckaway'))
 
-        self.assertTrue(result)
+    def test_area_regex_false(self):
+        result = route_finder.regex('pawtuckaway')
+        self.assertIsNone(result.search('cannon'))
 
 
 if __name__ == '__main__':
