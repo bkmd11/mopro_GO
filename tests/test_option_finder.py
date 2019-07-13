72a426c3db1642b598977299f0946632a7ce9f1f
bkmd11 <bkendall11@gmail.com>
2019-07-13 14:09:45 -0400

    fixed my tests

--- tests/test_option_finder.py
+++ tests/test_option_finder.py
@@ -25,37 +25,35 @@ class TestOptionFinder(unittest.TestCase):
         self.assertIn('pawtuckaway', result)
         self.assertIn('cannon-cliff', result)
 
-    # TODO: Fix these tests too
     # Testing that the sub_area options works
     def test_sub_area_options_works(self):
-        result = option_finder.sub_area_options('cannon-cliff', self.json_file)
+        result = option_finder.sub_area_options([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 
-        self.assertEqual(len(result), 1)
+        self.assertEqual(result, [3, 6, 9])
 
     # Tests that it indexes my list correctly
     def test_sub_area_options_returns_right_thing(self):
-        result = option_finder.sub_area_options('cannon-cliff', self.json_file)
+        result = option_finder.sub_area_options([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 
-        self.assertEqual(result, ['big-wall'])
+        self.assertEqual(result, [3, 6, 9])
 
-    # todo fix grade_option tests
     # Tests my grade_option finder pulls the right grade in a given area
     def test_grade_options_area(self):
-        result = option_finder.grade_options('cannon-cliff', self.json_file)
+        result = option_finder.grade_options([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 
-        self.assertEqual(result, ['5.11c '])
+        self.assertEqual(result, [3, 6, 9])
 
     # Tests that grade_options will only pull from one sub_area under a given area
     def test_grade_options_sub_area(self):
-        result = option_finder.grade_options('upper-cliff', self.json_file)
+        result = option_finder.grade_options([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 
-        self.assertEqual(result, ['5.9+ '])
+        self.assertEqual(result, [3, 6, 9])
 
     # Tests that it will pull all the grade options from an area
     def test_grade_options_returns_multiple_grades(self):
-        result = option_finder.grade_options('pawtuckaway', self.json_file)
+        result = option_finder.grade_options([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 
-        self.assertEqual(len(result), 6)
+        self.assertEqual(result, [3, 6, 9])
 
 
 if __name__ == '__main__':
